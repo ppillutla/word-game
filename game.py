029ed595438c18b39973741091f52dbe20d59401
ppillutla <pallavi.pillutla@gmail.com>
2022-01-19 16:29:55 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,14 @@
 points = 0
-target = "" # TODO: pull from a dictionary API
+target = ""  # TODO: pull from a dictionary API
+
+check_types = {
+    'naa': 0,
+    'ehh': 1,
+    'yer': 2
+}
+# todo use a real dict eventually, this is a placeholder
+dictionary = ['router', 'pythons', 'amazing', 'jukebox', 'torment', 'popular', 'trample']
+
 
 def driver(word):
     if len(word) == 7:
@@ -7,29 +16,44 @@ def driver(word):
     elif 2 < len(word) < 7:
         checkContainsWord(word)
     else:
-        error()
+        raise Exception('Invalid length')
 
 
-def checkWord(word):
-    if word == target:
-        return True
-    # check if any letters are correctly placed 
-        # add to marked letters list
-    # check if any letters are correct but incorrectly placed 
-        # add to marked letters list
-    # return marked letters 
-    return
+def checkWord(guessed_word):
+    if guessed_word in dictionary:
+        guess_marked_up = []
+
+        if guessed_word == target:
+            for c in guessed_word:
+                guess_marked_up.append([c, check_types['yer']])
+            return guess_marked_up
+
+        else:
+            for i in range(len(guessed_word)):
+                if guessed_word[i] == target[i]:
+                    guess_marked_up.append([guessed_word[i], check_types['yer']])
+                elif guessed_word[i] in target:
+                    guess_marked_up.append([guessed_word[i], check_types['ehh']])
+                else:
+                    guess_marked_up.append([guessed_word[i], check_types['naa']])
+
+            return guess_marked_up
+    else:
+        raise Exception('Word not in dictionary')
+
 
 def checkContainsWord(word):
     # check if the word can be made using the letters in target
-    # if yes: 
+    # if yes:
     #   add 1 point per letter
-    # if no: 
+    # if no:
     #   return errorNotContained
     return
 
+
 def error():
     return
+
 
 def errorNotContained():
     return
